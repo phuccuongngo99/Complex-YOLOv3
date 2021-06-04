7ccfab738b7b486cdb833f5af165e7c1ae07ea28
Ngo Phuc Cuong <41280374+phuccuongngo99@users.noreply.github.com>
2021-06-04 10:56:08 +0800

    Update train.py

--- train.py
+++ train.py
@@ -140,7 +140,7 @@ if __name__ == "__main__":
 
             model.seen += imgs.size(0)
 
-        if epoch % opt.evaluation_interval == 0:
+        if epoch % opt.evaluation_interval == 0 and epoch != 0:
             print("\n---- Evaluating Model ----")
             # Evaluate the model on the validation set
             precision, recall, AP, f1, ap_class = evaluate(
@@ -149,7 +149,7 @@ if __name__ == "__main__":
                 conf_thres=0.5,
                 nms_thres=0.5,
                 img_size=opt.img_size,
-                batch_size=8,
+                batch_size=opt.batch_size,
             )
             evaluation_metrics = [
                 ("val_precision", precision.mean()),
